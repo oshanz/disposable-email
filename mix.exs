7c301f67b255eb0c3e9354eec4b8c99003174926
Oshan Wisumperuma <warunaoshan@gmail.com>
2025-12-09 18:51:57 +1100

    chore: add licenses

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule DisposableEmail.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description:
+        "Detect disposable and temporary email addresses by checking domains against a regularly updated list from the disposable-email-domains repository.",
+      source_url: "https://github.com/oshanz/disposable-email",
+      licenses: "GNU GPLv3"
     ]
   end
 
