90d7a4e2b2b37e9374c6128f0bd5b702208b5d41
Oshan Wisumperuma <warunaoshan@gmail.com>
2026-02-02 01:45:10 +1100

    feat: removed deprecated is_disposable? function

--- test/disposable_email_test.exs
+++ test/disposable_email_test.exs
@@ -2,18 +2,8 @@ defmodule DisposableEmailTest do
   use ExUnit.Case
   doctest DisposableEmail
 
-  test "zed.eu is blocked [deprecated]" do
-    assert true == DisposableEmail.is_disposable?("dfs@z0d.eu")
-  end
-
   test "zed.eu is blocked" do
     assert true == DisposableEmail.disposable?("dfs@z0d.eu")
-  end
-
-  test "subdomains are blocked [deprecated]" do
-    assert true == DisposableEmail.is_disposable?("testmail@yopmail.com")
-    assert true == DisposableEmail.is_disposable?("testmail@level1.yopmail.com")
-    assert true == DisposableEmail.is_disposable?("testmail@level1.level2.yopmail.com")
   end
 
   test "subdomains are blocked" do
@@ -22,12 +12,8 @@ defmodule DisposableEmailTest do
     assert true == DisposableEmail.disposable?("testmail@level1.level2.yopmail.com")
   end
 
-  test "gmail.com is allowed [deprecated]" do
-    assert false == DisposableEmail.is_disposable?("dfs@gmail.com")
-  end
-
   test "gmail.com is allowed" do
-    assert false == DisposableEmail.is_disposable?("dfs@gmail.com")
+    assert false == DisposableEmail.disposable?("dfs@gmail.com")
   end
 
   test "download blocklist" do
