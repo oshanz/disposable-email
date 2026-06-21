2520e04debd36664ebcef7d5b7f59977be09d171
Oshan Wisumperuma <warunaoshan@gmail.com>
2026-06-21 22:15:01 +1000

    Bump version from 0.5.4 to 0.5.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DisposableEmail.MixProject do
   def project do
     [
       app: :disposable_email,
-      version: "0.5.4",
+      version: "0.5.5",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
